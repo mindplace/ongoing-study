0ab4b87fca9407eaf97a547bd2e61ec5c2c23445
Esther Leytush <eleytush@gmail.com>
2015-12-22 01:35:41 +0000

    updated folders

--- 02_factorial.rb
+++
@@ -1,6 +0,0 @@
-# Write a method factorial which takes a number and returns the 
-# product of every number up to the current number multiplied together.
-
-def factorial(num)
-    (1..num).to_a.inject(:*)
-end
