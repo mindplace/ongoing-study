6231989f87e8f97a6172cc4c2ee671e881d2fe06
Esther Leytush <eleytush@gmail.com>
2016-01-05 01:56:24 +0000

    Completed Greed game

---
+++ Greed game/Play_the_game.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+load 'GreedGame.rb'
+load 'GamePlay.rb'
+load 'Players.rb'
+load 'DiceSet.rb'
+
+new_game = GreedGame.new
+new_game.gameplay
+
+
+# changes I want to make:
+#
+# make it more chance a player will choose
+# to roll again if there's more dice available
+#
+# make language of the game more sensiive
+# to user and gender
