4230b8ce8c4eac7dc55fb010164f096974306d9b
Esther Leytush <eleytush@gmail.com>
2016-01-07 15:45:30 +0000

    minor changes

--- Greed game/Play_the_game.rb
+++ Greed game/Play_the_game.rb
@@ -11,8 +11,12 @@ new_game.gameplay
 
 # changes I want to make:
 #
-# make it more chance a player will choose
-# to roll again if there's more dice available
+# - make it more chance a player will choose
+#   to roll again if there's more dice available
 #
-# make language of the game more sensiive
-# to user and gender
+# - make language of the game more sensiive
+#   to user and gender
+#
+# - making the program more into an app, maybe
+#   through Heroku? Just something I can link
+#   to in my GitHub so people can try it out. 
